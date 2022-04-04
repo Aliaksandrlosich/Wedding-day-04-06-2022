5eb3576f35a415401924f6080b111d405e847a64
alexandrLosich <alexandr.losich@iptel.co>
2022-04-04 20:13:48 +0300

    Add first react layout

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import png from './oops.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <img src={png} alt='Смешная картинка'/>
+    <p>Упсс! Подождите пожалуйста! Мы ещё не закончили настройку странички</p>
+    </div>
+  );
+}
+
+export default App;
