bc56d097a9a9b4f326db07b07e4611e93bde3a9a
alexandrLosich <alexandr.losich@iptel.co>
2022-04-09 03:42:34 +0300

    -Update hover in number link

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -5,19 +5,19 @@ export default function Contact () {
  return (
   <div>
    <svg className='intro' viewBox='-20 0 400 30'>
-    <text textAnchor='start' x='10' y='20' className='contact__text text-stroke' clipPath='url(#contact__promo_text)'>Наши номера телефонов:</text>
-    <text textAnchor='start' x='10' y='20' className='contact__text text-stroke text-stroke-2' clipPath='url(#contact__promo_text)'>Наши номера телефонов:</text>
+    <text className='contact__text text-stroke' textAnchor='start' x='10' y='20' clipPath='url(#contact__promo_text)'>Наши номера телефонов:</text>
+    <text className='contact__text text-stroke text-stroke-2' textAnchor='start' x='10' y='20' clipPath='url(#contact__promo_text)'>Наши номера телефонов:</text>
     <defs>
      <clipPath id='contact__promo_text'>
-      <text textAnchor='start' x='10' y='20' className='contact__text'>Наши номера телефонов:</text>
+      <text className='contact__text' textAnchor='start' x='10' y='20'>Наши номера телефонов:</text>
      </clipPath>
     </defs>
    </svg>
    <div className='contacts__panel'>
     <p className='contact__number-label'>Номер Жениха (если кто-то забыл, то Александр)</p>
-    <a href='tel:+375291146069' className='contact__number-link'>+375(29)114-60-69</a>
+    <a className='contact__number-link' href='tel:+375291146069'>+375(29)114-60-69</a>
     <p className='contact__number-label'>Номер Невесты (если кто-то забыл, то Яна)</p>
-    <a href='tel:+375445511104' className='contact__number-link'>+375(44)551-11-04</a>
+    <a className='contact__number-link' href='tel:+375445511104'>+375(44)551-11-04</a>
    </div>
   </div>
  )
