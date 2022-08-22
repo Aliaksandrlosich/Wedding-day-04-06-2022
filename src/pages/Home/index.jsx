02b7885848be0a3be76643c99fb7c9cb84c797e9
alexandrLosich <alexandr.losich@iptel.co>
2022-08-22 21:25:05 +0300

    Update photo link in home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -13,6 +13,8 @@ export default function Home () {
    <p className='photo-page__text'>По возможности, не приносите живые букеты цветов! Через пару дней, мы уедем в Минск, и не успеем насладиться ими,
     а так же забрать все с собой. Если ходите что-то вручить, можете подарить что-то другое. Мягкие игрушки, сладости,
     алкоголь, и т.д.</p>
+    <p className='contact__number-label'> Фотографии Готовы!</p>
+    <a className='photo__link' href='https://disk.yandex.by/d/KjYUU-pltn1MVg' target="_blank">Ссылка на фотографии</a>
   </div>
  )
 }
